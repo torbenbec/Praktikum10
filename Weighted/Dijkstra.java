33bfff6163df54a5af75f1fa5cc22a87043a7395
torbenbec <105551506+torbenbec@users.noreply.github.com>
2022-06-17 10:05:16 +0200

    fertig

--- Weighted/Dijkstra.java
+++ Weighted/Dijkstra.java
@@ -11,19 +11,25 @@ public class Dijkstra {
             System.out.println("Eingabe war nicht eine Zahl");
         }
         WeightedGraph graph = WeightedGraph.fromFile(path);
-	int ergebniss=dijkstra( graph,  s,  t);
-	if(ergebniss==Integer.MAX_VALUE){
-		System.out.println("Es gibt keinen Pfad von Knoten "+s+" zu Knoten " +t+".");
-	}else{ 
-		System.out.println("Kuerzester Pfad der Laenge " +ergebniss+" gefunden.");
+	int a=graph.getNumNodes();
+        //sind s und t im Graphen
+        if(t>=0&&s>=0&&s<a&&t<a){
+		int ergebniss=dijkstra( graph,  s,  t);
+		if(ergebniss==Integer.MAX_VALUE){
+			System.out.println("Es gibt keinen Pfad von Knoten "+s+" zu Knoten " +t+".");
+		}else{ 
+			System.out.println("Kuerzester Pfad der Laenge " +ergebniss+" gefunden.");
+		}
 	}
     }
        public  static int dijkstra(WeightedGraph g, int s, int t){
         	int[] dist =new int[g.getNumNodes()+1];
+	       // alle distanzen auf Integer.MAX_VALUE setzen
         	for(int i=1; i<=g.getNumNodes();i++){
         		dist[i]=Integer.MAX_VALUE;
         	}
         	PriorityQueue<HeapElement> queue= new PriorityQueue<HeapElement>();
+	       // s der queue hinzufügen
         	HeapElement sheap= new HeapElement(0, g.getNode(s));
         	
         	queue.add(sheap);
@@ -31,6 +37,7 @@ public class Dijkstra {
         	while(!queue.isEmpty()){
         		HeapElement u1=queue.remove();
         		PriorityQueue<HeapElement> hilfsqueue= new PriorityQueue<HeapElement>();
+			// Element mit geringstem Abstand finden
         		while (!queue.isEmpty()) {
         			HeapElement u2=queue.remove();
         			if(dist[u2.getNode().getId()] == Integer.MAX_VALUE) {
@@ -45,12 +52,14 @@ public class Dijkstra {
             		}
             		queue=hilfsqueue;
             		dist[u1.getNode().getId()]=u1.getDistance();
+			// schon distanz von t gefunden?
             		if(u1.getNode().getId()==t){
             			return dist[t];
             		}
             		LinkedList<WeightedEdge> edges = u1.getNode().getEdges();
+			//alle Nodes mit denen u1 verbunden ist und die noch nicht berechnet wurden in die queue einfügen
             		for(WeightedEdge edge: edges){
-               		if(dist[edge.getDst().getId()] == Integer.MAX_VALUE) {
+               			if(dist[edge.getDst().getId()] == Integer.MAX_VALUE) {
             				HeapElement wheap= new HeapElement(u1.getDistance()+edge.getWeight(),edge.getDst());
         				queue.add(wheap);
         			}
